d28ceeec999123e594cb15b4d4235f87ac946995
jasonfilips <jasonfilipss@yahoo.com>
2023-09-21 01:27:40 +0300

    Created the main game loop

--- tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe.cpp
@@ -5,23 +5,23 @@
 using namespace std;
 
 char digits[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
-int row;
-int column;
 char token = 'x';
 bool tie = false;
 
 void DrawTable(char digits[3][3]) {
     cout << "\n";
-    cout << "  " << digits[0][0] << " | " << digits[0][1] << " | " << digits[0][2] << " \n";
-    cout << " ___|___|___ \n";
-    cout << "  " << digits[1][0] << " | " << digits[1][1] << " | " << digits[1][2] << " \n";
-    cout << " ___|___|___ \n";
-    cout << "  " << digits[2][0] << " | " << digits[2][1] << " | " << digits[2][2] << " \n";
-    cout << "    |   |   \n";
+    cout << "  " << digits[0][0] << " | " << digits[0][1] << " | " << digits[0][2] << endl;
+    cout << " ___|___|___" << endl;
+    cout << "  " << digits[1][0] << " | " << digits[1][1] << " | " << digits[1][2] << endl;
+    cout << " ___|___|___" << endl;
+    cout << "  " << digits[2][0] << " | " << digits[2][1] << " | " << digits[2][2] << endl;
+    cout << "    |   |   " << endl;
 }
 
 void GameLoop(char digits[3][3]) {
 
+    int row;
+    int column;
     int digit;
 
     if (token == 'x') {
@@ -29,7 +29,67 @@ void GameLoop(char digits[3][3]) {
         cin >> digit;
     }
 
+    if (token == '0') {
+        cout << "Please Enter";
+        cin >> digit;
+    }
+
+    if (digit == 1) {
+        row = 0;
+        column = 0;
+    }
+    else if (digit == 2) {
+        row = 0;
+        column = 1;
+    }
+    else if (digit == 3) {
+        row = 0;
+        column = 2;
+    }
+    else if (digit == 4) {
+        row = 1;
+        column = 0;
+    }
+    else if (digit == 5) {
+        row = 1;
+        column = 1;
+    }
+    else if (digit == 6) {
+        row = 1;
+        column = 2;
+    }
+    else if (digit == 7) {
+        row = 2;
+        column = 0;
+    }
+    else if (digit == 8) {
+        row = 2;
+        column = 1;
+    }
+    else if (digit == 9) {
+        row = 2;
+        column = 2;
+    }
+
+    else if (digit < 1 || digit > 9) {
+        cout << "Wrong digit" << endl;
+    }
+
+    if (token == 'x' && digits[row][column] != 'x' && digits[row][column] != '0') {
+        digits[row][column] = 'x';
+        token = '0';
+    }
+    else if (token == '0' && digits[row][column] != 'x' && digits[row][column] != '0') {
+        digits[row][column] = '0';
+        token = 'x';
+    }
+    else {
+        cout << "That's a wrong space" << endl;
+        GameLoop(digits);
+    }
+
     DrawTable(digits);
+    GameLoop(digits);
 }
 
 int main()
